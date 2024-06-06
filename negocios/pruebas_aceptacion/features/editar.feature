0ee296e3802408a34822bb5c5f985e901dcdf111
AngieCard19 <94593826+AngieCard19@users.noreply.github.com>
2024-06-06 13:17:03 -0600

    Subiendo cambios

--- negocios/pruebas_aceptacion/features/editar.feature
+++ negocios/pruebas_aceptacion/features/editar.feature
@@ -1,14 +1,16 @@
-Característica: Eliminar
+Característica: Editar
 Como usuario del sistema Altas MFC 
-quiero poder eliminar los equipos y ordenes
-para poder conservar espacio
+quiero poder editar los equipos y ordenes
+para tener la información correcta
 
-Escenario: Eliminar el equipo con sn A45789
+Escenario: Editar equipo
 Dado que ingreso a la url "http://localhost:8000/"
 Y escribo mi usuario "AdminMFC" y mi Password "1234" 
 Y presiono el botón Login 
 Y dirijete Menu
 Y presiono Equipos
 Y presiono Lista de Equipos
-Cuando presiono el botón eliminar
+Y presiono el botón editar con el numero de serie "A45789"
+Y cambio el valor del modelo a "Victus 15"
+Cuando presiono el boton Guardar
 Entonces el sistema mostrara las listas con el mensaje "Lista"
--- negocios/pruebas_aceptacion/features/steps/eliminar.py
+++ negocios/pruebas_aceptacion/features/steps/eliminar.py
@@ -5,23 +5,12 @@ import time
 
 @given(u'presiono Lista de Equipos')
 def step_impl(context):
-    context.driver.find_element(By.XPATH, '//*[@id="forms-nav"]/li[2]/a')
+    context.driver.find_element(By.XPATH, '/html/body/aside/ul/li[4]/ul/li[2]/a')
 
 
-@when(u'presiono el botón eliminar con el numero de serie "{sn}"')
-def step_impl(context,sn):
-   tabla=context.driver.find_element(By.CLASS_NAME,'table')
-   tbody= tabla.context.driver.find_element(By.TAG_NAME,'tbody')
-   rows=tbody.context.driver.find_element(By.TAG_NAME, 'tr')
-   
-   for row in rows:
-        campos=row.context.driver.find_element(By.TAG_NAME, 'td')
-        for campo in campos:
-            if sn in campo.text:
-                delete_button = row.find_element(By.CLASS_NAME, 'btn btn-danger')
-                delete_button.click()
-                return          
-   
+@when(u'presiono el botón eliminar')
+def step_impl(context):
+   context.driver.find_element(By.XPATH, '//*[@id="main"]/table/tbody/tr[1]/td[7]/a').click()      
    
    
 
