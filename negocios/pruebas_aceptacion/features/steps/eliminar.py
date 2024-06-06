3eebe73f674736954fe361797ed398e2c70e745f
AngieCard19 <94593826+AngieCard19@users.noreply.github.com>
2024-06-06 10:10:08 -0600

    Eliminar

--- negocios/pruebas_aceptacion/features/steps/eliminar.py
+++ negocios/pruebas_aceptacion/features/steps/eliminar.py
@@ -11,9 +11,23 @@ def step_impl(context):
 @when(u'presiono el botón eliminar con el numero de serie "{sn}"')
 def step_impl(context,sn):
    tabla=context.driver.find_element(By.CLASS_NAME,'table')
-   tbody= tabla.context.driver.find_element(By.CLASS_NAME,'tbody')
+   tbody= tabla.context.driver.find_element(By.TAG_NAME,'tbody')
+   rows=tbody.context.driver.find_element(By.TAG_NAME, 'tr')
+   
+   for row in rows:
+        campos=row.context.driver.find_element(By.TAG_NAME, 'td')
+        for campo in campos:
+            if sn in campo.text:
+                delete_button = row.find_element(By.CLASS_NAME, 'btn btn-danger')
+                delete_button.click()
+                return          
+   
+   
    
 
-@then(u'el sistema mostrara las listas con el mensaje "Lista"')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then el sistema mostrara las listas con el mensaje "Lista"')
+@then(u'el sistema mostrara las listas con el mensaje "{lista}"')
+def step_impl(context,lista):
+    head= context.driver.find_element(By.ID, 'main')
+    time.sleep(5)
+    assert lista in head
+    
