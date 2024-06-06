f4f2b3e35a52dc5a955f0189d81817c80b751af6
Andrea-isa <35165623@uaz.edu.mx>
2024-06-05 19:55:42 -0600

    Test de Forms

---
+++ negocios/inicio/test/tests_forms.py
@@ -0,0 +1,57 @@
+from django.test import TestCase
+from inicio.forms import UserForm
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+class UserFormTest(TestCase):
+
+    def test_user_form_valid(self):
+        form = UserForm(data={
+            'username': 'usuario',
+            'email': 'usuario@example.com',
+            'password': 'password123',
+            're_pass': 'password123'
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_user_form_invalid_password_mismatch(self):
+        form = UserForm(data={
+            'username': 'usuario',
+            'email': 'usuario@example.com',
+            'password': 'password123',
+            're_pass': 'password456'
+        })
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['password'], ['Las contraseñas no son iguales'])
+
+    def test_user_form_save(self):
+        form = UserForm(data={
+            'username': 'usuario',
+            'email': 'usuario@example.com',
+            'password': 'password123',
+            're_pass': 'password123'
+        })
+        self.assertTrue(form.is_valid())
+        user = form.save()
+        self.assertIsInstance(user, User)
+        self.assertEqual(user.username, 'usuario')
+        self.assertTrue(user.check_password('password123'))
+
+    def test_user_form_save_with_commit_false(self):
+        form = UserForm(data={
+            'username': 'usuario',
+            'email': 'usuario@example.com',
+            'password': 'password123',
+            're_pass': 'password123'
+        })
+        self.assertTrue(form.is_valid())
+        user = form.save(commit=False)
+        self.assertIsInstance(user, User)
+        self.assertEqual(user.username, 'usuario')
+        self.assertTrue(user.check_password('password123'))
+        self.assertIsNone(user.id)  # El usuario aún no se ha guardado en la base de datos
+        user.save()
+        self.assertIsNotNone(user.id)  # El usuario ahora está guardado en la base de datos
+    
+
+
